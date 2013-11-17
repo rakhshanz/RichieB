3f65f4244cf444043067cbb93e5a06fe7d698c12
Rakhshan Zahid <rakhshanzahid@RAKHSHANs-MacBook-Pro.local>
2013-11-17 14:33:53 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Richieb::Application.config.secret_key_base = '79af837d881790ee5c2c9af399c841cc771a0fcebca3d5b4de98f76a10b5cc60275cb6aac9e2bff0c1c888947f8a7041583a1eaec0a66234811fc0acd8d3a645'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Richieb::Application.config.session_store :cookie_store, key: '_richieb_session'
